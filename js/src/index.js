fc2a20bf186a8adc3cc57161f9e7e9527903cd92
iamllitog <litong.forme@gmail.com>
2015-04-03 23:48:00 +0800

    完成head(除了不好看)

--- js/src/index.js
+++ js/src/index.js
@@ -1,9 +1,12 @@
 avalon.config({
 	paths: {
 		jquery: "../../framework/jquery.min.js",
-        bootstrap : '../../framework/bootstrap/js/bootstrap.min.js'
-	},
+                          bootstrap : '../../framework/bootstrap/js/bootstrap.min.js'
+    },
     shim: {
+        "jquery": {
+            exports: "$"
+        },
         "bootstrap": {
             deps: ["jquery"],
             exports: "$.fn.popover"
@@ -12,8 +15,12 @@ avalon.config({
     enforceDefine: true
 });
 
-require(['jquery','bootstrap','domReady!'],function ($) {
+require(['jquery','bootstrap','domReady!'],function () {
 	avalon.define("mean", function(vm){
-	});
+                    vm.loadHead = function () {
+                        $('#head_homeBt').addClass('active');
+                    };
+
+            });
 	avalon.scan();
 });
