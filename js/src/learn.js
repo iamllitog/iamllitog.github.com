f7b6d26fd339a67f4650c239c84c7be2051af36e
litong <litong.forme@gmail.com>
2015-04-13 17:42:16 +0800

    增加学习生活

---
+++ js/src/learn.js
@@ -0,0 +1,24 @@
+avalon.config({
+	paths: {
+		jquery: "../../framework/jquery.min.js",
+                          bootstrap : '../../framework/bootstrap/js/bootstrap.min.js'
+    },
+    shim: {
+        "jquery": {
+            exports: "$"
+        },
+        "bootstrap": {
+            deps: ["jquery"]
+        }
+    }
+});
+
+require(['jquery','bootstrap','domReady!'],function () {
+	avalon.define("mean", function(vm){
+                    vm.loadHead = function () {
+                        $('#head_learnBt').addClass('active');
+                    };
+
+            });
+	avalon.scan();
+});
