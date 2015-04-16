9a23d9672b9bc7c9887598e3bd18eb8ca980aebd
litong <litong.forme@gmail.com>
2015-04-16 16:59:20 +0800

    提交着玩

---
+++ js/src/aboutme.js
@@ -0,0 +1,24 @@
+avalon.config({
+	paths: {
+		jquery: "../../framework/jquery.min.js",
+                          bootstrap : '../../framework/bootstrap/js/bootstrap.min.js'
+    },
+    shim: {
+        "jquery": {
+            exports: "$"
+        },
+        "bootstrap": {
+            deps: ["jquery"]
+        }
+    }
+});
+
+require(['jquery','bootstrap','domReady!'],function () {
+	avalon.define("mean", function(vm){
+                    vm.loadHead = function () {
+                        headPart.initHead('head_aboutmeBt');
+                    };
+
+            });
+	avalon.scan();
+});
